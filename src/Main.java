deb6b3acc40af31ecd400abd094024b723f7f732
jasper <whiteana001@warren.k12.in.us>
2025-02-10 13:11:45 -0500

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
+    public static void main(String[] args) {
+        List<String> arrayList = new ArrayList<>();
+        arrayList.add("Triangle");
+        arrayList.add("Rectangle");
+        arrayList.add("Circle");
+        arrayList.add("Rhombus");
+        arrayList.add("Square");
+        arrayList.add("Oval");
+        arrayList.add("Trapezoid");
+        System.out.println("Shapes: " + arrayList);
+
+        arrayList.remove(3);
+        arrayList.sort(null);
+        System.out.print(arrayList);
+    }
 }
